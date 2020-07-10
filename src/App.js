c8effebc8e3abbb3ae9f5fa31c23f8034b933119
chenweiyan <chanweiyan007@gmail.com>
2020-07-10 16:04:29 +0800

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+/**
+ * function - Route
+ */
+function Index() {
+  return <p>This is home page.</p>
+}
+
+function About() {
+  return <p>This is about page.</p>
+}
+
+function Users() {
+  return <p>This is users page.</p>
+}
+
+/**
+ * function - Router App
+ * @returns {bollean}
+ * @constructor
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
